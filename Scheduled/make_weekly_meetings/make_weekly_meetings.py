7babcd9070317f721ef999a73b50d522ab415322
ambiguousname <tyly04@gmail.com>
2022-09-03 10:26:03 -0700

    Fixing timezone issues with Monday.com

--- Scheduled/make_weekly_meetings/make_weekly_meetings.py
+++ Scheduled/make_weekly_meetings/make_weekly_meetings.py
@@ -1,5 +1,6 @@
 import json
 import datetime
+import pytz
 
 from make_weekly_meetings.monday_task_creation import create_meeting_task, has_recent_meeting_task, setup_monday
 from make_weekly_meetings.make_weekly_docs import get_if_meeting_doc_exists, copy_doc, extract_file_id
@@ -17,7 +18,13 @@ from make_weekly_meetings.make_weekly_calendars import calendar_has_event, add_e
 # or https://drive.google.com/drive/u/0/folders/ID
 # And just copy and pasting the ID part. Should work for slides or docs. You might have to expand scopes for other files if you want those duplicated.
 
-# Get the current monday of this week. Add or subtract days as needed.
+# For conversion from and to UTC and Pacific Time. We're in Santa Cruz, so the times we're given will be in terms of the Olson timezone America/Los_Angeles.
+# Since this could be running on a webserver located elsewhere, we have to make sure it'll convert to UTC.
+# And we can't just use a static constant because of stupid daylight savings.
+western = pytz.timezone("America/Los_Angeles")
+utc = pytz.utc
+
+# Get the current monday of this week in terms of Santa Cruz time. Add or subtract days as needed.
 def get_week_monday(offset="Monday"):
     offsets = {
         "monday": 0,
@@ -30,7 +37,7 @@ def get_week_monday(offset="Monday"):
         "nextweeksunday": 6
     }
 
-    today = datetime.date.today()
+    today = western.localize(datetime.datetime.now())
     return today + datetime.timedelta(days=-(today.weekday())) + datetime.timedelta(days=offsets[offset.lower()])
 
 def init_meeting_creation():
@@ -41,9 +48,11 @@ def make_meetings(service, settings):
     for doc in settings["docsToCopy"]:
         docSettings = settings["docsToCopy"][doc]
 
+        # TODO: Re-write this to just use one date that includes the specific date + time (if available in the settings).
         date = get_week_monday("Sunday")
         if "date" in docSettings:
             date = get_week_monday(docSettings["date"]["day"])
+        date_utc = utc.normalize(date.astimezone(utc))
 
         month = str(date.month)
         day = str(date.day)
@@ -51,7 +60,17 @@ def make_meetings(service, settings):
             month = "0" + month
         if len(day) == 1:
             day = "0" + day
+
+        month_utc = str(date_utc.month)
+        day_utc = str(date_utc.day)
+        if len(month_utc) == 1:
+            month_utc = "0" + month_utc
+        if len(day_utc) == 1:
+            day_utc = "0" + day_utc
+
         date_string = f"{date.year}/{month}/{day}"
+        # In case some APIs require UTC:
+        date_string_utc = f"{date_utc.year}/{month_utc}/{day_utc}"
 
         name = f"[{date_string}] {doc}"
         # If we're currently allowed to make the document, and if the document we're trying to make doesn't exist:
@@ -68,11 +87,22 @@ def make_meetings(service, settings):
             if "monday.com" in docSettings and not has_recent_meeting_task(settings["monday.com"], docSettings["monday.com"]["name"], name):
                 kwargs = {}
                 if "date" in docSettings:
+                    # Use UTC:
                     kwargs["date"] = {
-                        "date": date_string.replace("/", "-")
+                        "date": date_string_utc.replace("/", "-")
                     }
+
                     if "time" in docSettings["date"]:
-                        kwargs["date"]["time"] = docSettings["date"]["time"]
+                        # TODO: Move this conversion up, as with the TODO above.
+                        time_split = docSettings["date"]["time"].split(":")
+                        time_utc = utc.localize(datetime.datetime(year=date.year, month=date.month, day=date.day, hour=int(time_split[0]), minute=int(time_split[1]), tzinfo=western))
+                        hour = str(time_utc.hour)
+                        minute = str(time_utc.minute)
+                        if len(hour) == 1:
+                            hour = "0" + hour
+                        if len(minute) == 1:
+                            minute = "0" + minute
+                        kwargs["date"]["time"] = hour + ":" + minute
                 
                 if "teamId" in docSettings["monday.com"]:
                     kwargs["team_id"] = docSettings["monday.com"]["teamId"]
